670f3b18b94dff293403ffad17d487be79da54ca
RRoggia <renanzr@gmail.com>
2019-12-10 18:24:00 -0300

    implement fuel requirement calculator

--- src/main/java/rroggia/github/io/day1/FuelRequirementCalculator.java
+++ src/main/java/rroggia/github/io/day1/FuelRequirementCalculator.java
@@ -1,12 +1,21 @@
 package rroggia.github.io.day1;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FuelRequirementCalculator {
+	private List<Module> modules;
 
 	public FuelRequirementCalculator(Module... module) {
+		modules = Arrays.asList(module);
 	}
 
-	public Object calculateFuelRequirements() {
-		return 2;
+	public int calculateFuelRequirements() {
+		int fuelRequirement = 0;
+		for (var module : modules) {
+			fuelRequirement += module.determineFuelRequiredToLaunch();
+		}
+		return fuelRequirement;
 	}
 
 }
