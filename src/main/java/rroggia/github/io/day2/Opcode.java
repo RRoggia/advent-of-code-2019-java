12ed8b777690c1fb723799b2b778207b183c4c0e
RRoggia <renanzr@gmail.com>
2019-12-18 17:43:21 -0300

    add intitial solution

---
+++ src/main/java/rroggia/github/io/day2/Opcode.java
@@ -0,0 +1,10 @@
+package rroggia.github.io.day2;
+
+public class Opcode {
+
+	@Override
+	public String toString() {
+		return "1,9,10,3";
+	}
+
+}
---
+++ src/main/java/rroggia/github/io/day2/OpcodeReader.java
@@ -0,0 +1,14 @@
+package rroggia.github.io.day2;
+
+public class OpcodeReader {
+
+	public OpcodeReader(String opcodes) {
+	}
+
+	public Opcode getNextOpcode() {
+		return new Opcode();
+	}
+	
+	
+
+}
