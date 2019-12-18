65fa5567751b1667def73b86beafff873aed0bf5
RRoggia <renanzr@gmail.com>
2019-12-18 18:26:16 -0300

    rename class to follow java convention

--- src/test/java/rroggia/github/io/day2/OpcodeReaderTest.java
+++ src/test/java/rroggia/github/io/day2/OpcodeReaderTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class OpcodeTest {
+public class OpcodeReaderTest {
 
 	@Test
 	public void readOpcode() {
