5aea9d34422f64ee3c7979232e1993a720490267
RRoggia <renanzr@gmail.com>
2019-12-19 09:52:26 -0300

    add test for solving all opcodes

--- src/test/java/rroggia/github/io/day2/OpcodeReaderTest.java
+++ src/test/java/rroggia/github/io/day2/OpcodeReaderTest.java
@@ -54,4 +54,11 @@ public class OpcodeReaderTest {
 		assertEquals("3500,9,10,70,2,3,11,0,99,30,40,50", opcodeReader.getOpcodeCurrentState());
 	}
 
+	@Test
+	public void resolveAllOpcodes() {
+		var opcodeReader = new OpcodeReader("1,9,10,3,2,3,11,0,99,30,40,50");
+		opcodeReader.resolveAllOpcodes();
+		assertEquals("3500,9,10,70,2,3,11,0,99,30,40,50", opcodeReader.getOpcodeCurrentState());
+	}
+
 }
