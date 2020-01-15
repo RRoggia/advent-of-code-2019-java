b39740ecff5d1e275e8b1673f75d36c72c704b63
RRoggia <renanzr@gmail.com>
2020-01-15 10:32:31 -0300

    change consumers to use the grid connection determiner class

--- src/main/java/rroggia/github/io/day3/Day3.java
+++ src/main/java/rroggia/github/io/day3/Day3.java
@@ -4,17 +4,12 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Day3 {
 
 	private static final String RESOURCES_FOLDER = "src/main/resources";
-	private static final char UP = 'U';
-	private static final char DOWN = 'D';
-	private static final char RIGHT = 'R';
-	private static final char LEFT = 'L';
 
 	public static void main(String[] args) {
 
@@ -34,52 +29,7 @@ public class Day3 {
 	}
 
 	public static Set<String> determinePointsWireIsConnectedInTheGrid(String wireTraces) {
-		var connectionsInTheGrid = new HashSet<String>();
-
-		String[] traces = wireTraces.split(",");
-
-		int axisX = 0;
-		int axisY = 0;
-
-		connectionsInTheGrid.add(axisX + "," + axisY);
-
-		if (!(traces[0].isBlank() || traces[0].isEmpty())) {
-			for (String trace : traces) {
-				var direction = trace.charAt(0);
-				var moviment = Integer.parseInt(trace.substring(1, trace.length()));
-
-				switch (direction) {
-				case UP:
-					for (int i = 1; i <= moviment; i++) {
-						axisX++;
-						connectionsInTheGrid.add(axisX + "," + axisY);
-					}
-					break;
-				case DOWN:
-					for (int i = 1; i <= moviment; i++) {
-						axisX--;
-						connectionsInTheGrid.add(axisX + "," + axisY);
-					}
-					break;
-				case RIGHT:
-					for (int i = 1; i <= moviment; i++) {
-						axisY++;
-						connectionsInTheGrid.add(axisX + "," + axisY);
-					}
-					break;
-				case LEFT:
-					for (int i = 1; i <= moviment; i++) {
-						axisY--;
-						connectionsInTheGrid.add(axisX + "," + axisY);
-					}
-					break;
-				default:
-					throw new RuntimeException("input or split is not right.");
-				}
-			}
-		}
-
-		return connectionsInTheGrid;
+		return GridConnectionDeterminer.getInstance().determineConnections(wireTraces);
 	}
 
 	private static List<String> determineIntersections(Set<String> firstWireTracesKeys,
--- src/main/java/rroggia/github/io/day3/GridConnectionDeterminer.java
+++ src/main/java/rroggia/github/io/day3/GridConnectionDeterminer.java
@@ -1,10 +1,16 @@
 package rroggia.github.io.day3;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class GridConnectionDeterminer {
 
 	private static final GridConnectionDeterminer INSTANCE = new GridConnectionDeterminer();
+
+	private static final char UP = 'U';
+	private static final char DOWN = 'D';
+	private static final char RIGHT = 'R';
+	private static final char LEFT = 'L';
 
 	private GridConnectionDeterminer() {
 		// override public constructor created by default from java
@@ -14,7 +20,53 @@ public class GridConnectionDeterminer {
 		return INSTANCE;
 	}
 
-	public Set<String> determineConnections(String traces) {
-		return Day3.determinePointsWireIsConnectedInTheGrid(traces);
+	public Set<String> determineConnections(String wireTraces) {
+		var connectionsInTheGrid = new HashSet<String>();
+
+		String[] traces = wireTraces.split(",");
+
+		int axisX = 0;
+		int axisY = 0;
+
+		connectionsInTheGrid.add(axisX + "," + axisY);
+
+		if (!(traces[0].isBlank() || traces[0].isEmpty())) {
+			for (String trace : traces) {
+				var direction = trace.charAt(0);
+				var moviment = Integer.parseInt(trace.substring(1, trace.length()));
+
+				switch (direction) {
+				case UP:
+					for (int i = 1; i <= moviment; i++) {
+						axisX++;
+						connectionsInTheGrid.add(axisX + "," + axisY);
+					}
+					break;
+				case DOWN:
+					for (int i = 1; i <= moviment; i++) {
+						axisX--;
+						connectionsInTheGrid.add(axisX + "," + axisY);
+					}
+					break;
+				case RIGHT:
+					for (int i = 1; i <= moviment; i++) {
+						axisY++;
+						connectionsInTheGrid.add(axisX + "," + axisY);
+					}
+					break;
+				case LEFT:
+					for (int i = 1; i <= moviment; i++) {
+						axisY--;
+						connectionsInTheGrid.add(axisX + "," + axisY);
+					}
+					break;
+				default:
+					throw new RuntimeException("input or split is not right.");
+				}
+			}
+		}
+
+		return connectionsInTheGrid;
+
 	}
 }
